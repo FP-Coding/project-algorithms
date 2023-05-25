acea59eb019d7e8bdfa037d46b77663126ef102b
Felipe Pinto <felipe14062000@gmail.com>
2023-05-25 16:05:03 -0300

    test: testando a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("certo", "15")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(15, 15)
+    result_odd_reversed = encrypt_message("certo", 15)
+    result_even_reversed = encrypt_message("certo", 14)
+    result_even = encrypt_message("certo", 4)
+    result_odd = encrypt_message("certo", 3)
+
+    assert result_odd_reversed == "otrec"
+    assert result_even_reversed == "otrec"
+    assert result_odd == "rec_ot"
+    assert result_even == "o_trec"
