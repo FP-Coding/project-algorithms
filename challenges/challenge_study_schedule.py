a76e1205b1630b9bcccec5806d11c556aae95afc
Felipe Pinto <felipe14062000@gmail.com>
2023-05-25 14:47:19 -0300

    feat: implementação da função study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time and target_time != 0:
+        return None
+    count = 0
+    for start, end in permanence_period:
+        if not start or not end or type(start) != int or type(end) != int:
+            return None
+        if start <= target_time <= end:
+            count += 1
+    return count
