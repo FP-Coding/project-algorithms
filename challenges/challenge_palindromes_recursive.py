67ccdfe20bd8f505407ebd0a85f7af57ef0a63a0
Felipe Pinto <felipe14062000@gmail.com>
2023-05-25 18:01:40 -0300

    feat: implementação da função is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    if word[low_index] == word[high_index]:
+        new_low = low_index + 1
+        new_high = high_index - 1
+        if new_low < new_high:
+            return is_palindrome_recursive(word, new_low, new_high)
+        else:
+            return True
+    else:
+        return False
+
+
+word = "jarara"
+print(is_palindrome_recursive(word, 0, len(word) - 1))
